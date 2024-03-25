3228433dc8a2ac13f3ae25105e75701721363703
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-03-25 16:20:12 +0700

    Update configs

--- Apps/use_cases/VTC.py
+++ Apps/use_cases/VTC.py
@@ -1,7 +1,8 @@
 import add_packages
 import config
 from pprint import pprint
-import os, json
+import os
+import yaml
 
 from my_prompts import (
     prompts_onlinica
@@ -18,31 +19,31 @@ from langchain_core.prompts import (
     HumanMessagePromptTemplate
 )
 
-#*=============================================================================
+# *=============================================================================
 # call from main
-with open("./my_configs/vtc.json", "r") as json_file:
-    vtc_data = json.load(json_file)
+with open("./my_configs/vtc.yaml", 'r') as file:
+    configs_vtc = yaml.safe_load(file)
 
 qdrant_instance_lectures_content = vector_stores.QdrantWrapper(
     qdrant_host=os.getenv("QDRANT_HOST"),
     qdrant_api_key=os.getenv("QDRANT_API_KEY"),
-    **vtc_data["vector_db"]["qdrant_instance_lectures_content"]
+    **configs_vtc["vector_db"]["qdrant_instance_lectures_content"]
 )
 
 qdrant_instance_courses_information = vector_stores.QdrantWrapper(
     qdrant_host=os.getenv("QDRANT_HOST"),
     qdrant_api_key=os.getenv("QDRANT_API_KEY"),
-    **vtc_data["vector_db"]["qdrant_instance_courses_information"]
+    **configs_vtc["vector_db"]["qdrant_instance_courses_information"]
 )
 
 qdrant_instance_faq = vector_stores.QdrantWrapper(
     qdrant_host=os.getenv("QDRANT_HOST"),
     qdrant_api_key=os.getenv("QDRANT_API_KEY"),
-    **vtc_data["vector_db"]["qdrant_instance_faq"]
+    **configs_vtc["vector_db"]["qdrant_instance_faq"]
 )
 
-#*=============================================================================
-system_message_onlinica = prompts_onlinica.system_message_onlinica
+# *=============================================================================
+system_message_onlinica = configs_vtc["prompts"]["system_message_onlinica"]
 
 prompt_onlinica = prompts.create_prompt_custom_agent_openai_tools(
     system_message_onlinica)
@@ -56,4 +57,3 @@ tools = [
 llm = chat_models.chat_openai
 agent = agents.MyAgent(prompt=prompt_onlinica, tools=tools,
                        agent_type="openai_tools", llm=llm)
-
