9e1ea51505a5fbec5574d63185e4eccc3737c263
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-05-06 20:56:50 +0700

    Update

--- Apps/my_langchain/document_loaders.py
+++ Apps/my_langchain/document_loaders.py
@@ -1,5 +1,9 @@
+from langchain_core.documents import Document
 from langchain_community.document_loaders.text import TextLoader
 from langchain_community.document_loaders.web_base import WebBaseLoader
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
 from langchain_community.document_loaders.pdf import UnstructuredPDFLoader
+
+def format_docs(docs: list[Document]):
+	return "\n\n".join(doc.page_content for doc in docs)
---
+++ README.md
@@ -0,0 +1,17 @@
+# Large Languge Model-powered applications
+
+## Indexing Flow
+
+![Indexing Flow](./diagrams/Indexing%20Flow.jpeg)
+
+## RAG Flow
+
+![RAG Flow](./diagrams/RAG%20Flow.jpeg)
+
+## Agent Flow
+
+![Agent Flow](./diagrams/Agent%20Flow.jpeg)
+
+## System Design
+
+![System Design](./diagrams/System%20Design.jpeg)
